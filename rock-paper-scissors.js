b8d4c5c114e67615a3365340e9cbc6d26f7005e9
Ondra Svec <ondrasvecveslo@gmail.com>
2023-02-22 09:37:07 +0100

    Add function returning computer's random choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,10 @@
 //randomly select rock, paper or scissors for the computer
+function getComputerChoice() {
+    let gameArr = ["Rock", "Paper", "Scissors"];
+    let randomChoice = gameArr[Math.floor(Math.random() * gameArr.length)];
+    return randomChoice;
+}
+console.log(getComputerChoice());
 //get player's selection as input
 //play one round
 //play multiple rounds
