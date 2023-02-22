ca367e7ebac85db71a80c777929f18de5f5d59d6
Ondra Svec <ondrasvecveslo@gmail.com>
2023-02-22 09:31:17 +0100

    Add pseudocode for the game

---
+++ rock-paper-scissors.js
@@ -0,0 +1,6 @@
+//randomly select rock, paper or scissors for the computer
+//get player's selection as input
+//play one round
+//play multiple rounds
+//display the winner of each round
+//display the winner of the game
