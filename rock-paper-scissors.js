222e961ac07612cd61a7b8ed11425ec64bc8fdfa
Ondra Svec <ondrasvecveslo@gmail.com>
2023-02-25 12:55:46 +0100

    Refactor the functions to work with buttons

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,53 +2,64 @@
 let numOfWinsUser = 0;
 let numOfWinsComp = 0;
 
+//add button event listeners
+const buttons = document.querySelectorAll('.rps');
+buttons.forEach(button => button.addEventListener('click', playRound));
+
 //randomly select rock, paper or scissors for the computer
 function getComputerChoice() {
-    let gameArr = ["Rock", "Paper", "Scissors"];
-    let randomChoice = gameArr[Math.floor(Math.random() * gameArr.length)];
-    return randomChoice;
+     let gameArr = ["Rock", "Paper", "Scissors"];
+     let randomChoice = gameArr[Math.floor(Math.random() * gameArr.length)];
+     return randomChoice;
 }
-
-//play multiple rounds
-function game(numOfRounds) {
-    //get user's input and turn it into a number
-    numOfRounds = parseInt(prompt("How many rounds would you like to play?"));
-    //play a specific number of rounds based on user's input
-    for (let i = 0; i < numOfRounds; i++) {
-        console.log(playRound());
-    }
-    //display the winner of the game
-    return ((numOfWinsUser > numOfWinsComp) ? `You Win ${numOfWinsUser} to ${numOfWinsComp}.`
-            : (numOfWinsComp > numOfWinsUser) ? `You Lose ${numOfWinsComp} to ${numOfWinsUser}.`
-            : `It's a draw.`);
-}
-console.log(game());
 
 //play one round
-function playRound(playerSelection, computerSelection) {
+function playRound(e, playerSelection, computerSelection) {
     //get player's selection as input
-    playerSelection = prompt('Enter "rock", "paper" or "scissors".');
-    //lower case the input
-    let playerSelectionLow = playerSelection.toLowerCase();
-    //capitalize the input
-    let playerSelectionCap = playerSelectionLow[0].toUpperCase() + playerSelectionLow.slice(1);
-    console.log(playerSelectionCap);
+    playerSelection = e.target.value;
     //invoke computer's choice
     computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    
-    //display the winner of each round based on each player's choice
-    if (playerSelectionCap === computerSelection) {
-        return `It's a tie!`;
+
+    //create a paragraph with the info about the current round
+    const para = document.createElement('p');
+    //stop the game when either player reaches 5 points
+    if (numOfWinsUser === 5 || numOfWinsComp === 5) {
+        gameOver();
+    //store the winner of each round based on each player's choice
+    } else if (playerSelection === computerSelection) {
+         para.textContent = `It's a tie!`;
     } else if (
-        playerSelectionCap === "Rock" && computerSelection === "Scissors"
-        || playerSelectionCap === "Paper" && computerSelection === "Rock"
-        || playerSelectionCap === "Scissors" && computerSelection === "Paper"
-        ) {
-        numOfWinsUser++;
-        return `You Win! ${playerSelectionCap} beats ${computerSelection}.`;
+         playerSelection === "Rock" && computerSelection === "Scissors"
+         || playerSelection === "Paper" && computerSelection === "Rock"
+         || playerSelection === "Scissors" && computerSelection === "Paper"
+         ) {
+         numOfWinsUser++;
+         console.log(numOfWinsUser);
+         para.textContent = `You Win! ${playerSelection} beats ${computerSelection}.`;
     } else {
-        numOfWinsComp++;
-        return `You Lose! ${computerSelection} beats ${playerSelectionCap}.`;
+         numOfWinsComp++;
+         console.log(numOfWinsComp);
+         para.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
     }
+    //append the paragraph to the display div
+    const displayDiv = document.querySelector('.display');
+    displayDiv.appendChild(para);
+}
+
+//stop the game once a player reaches 5 points
+function gameOver() {
+        const displayDiv = document.querySelector('.display');
+        while (displayDiv.firstChild) {
+            displayDiv.removeChild(displayDiv.lastChild);
+        }
+        //create a paragraph with the info about the current round
+        const para = document.createElement('p'); 
+        if (numOfWinsUser > numOfWinsComp) {
+            para.textContent = `You Win ${numOfWinsUser} to ${numOfWinsComp}.`;
+        } else if (numOfWinsComp > numOfWinsUser) {
+            para.textContent = `You Lose ${numOfWinsComp} to ${numOfWinsUser}.`;
+        } else {
+            para.textContent = "It's a draw!";
+        }
+        displayDiv.appendChild(para);
 }
